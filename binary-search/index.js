a8e16db5461cff3e1185d3fdd188932a32f26c47
Steven Berlanga <zabawaba99@gmail.com>
2017-10-28 10:47:31 -0700

    returning the index of the item that was found

--- binary-search/index.js
+++ binary-search/index.js
@@ -1,14 +1,21 @@
 
-function searchRecursive(arr, val) {
+function searchRecursive(arr, start, end, val) {
   // if we have an empty arr, the value is not found
-  if (!arr || arr.length === 0) return false;
+  if (!arr || arr.length === 0) return -1;
 
-  // if we only have one thing, check if it's what we want
-  if (arr.length === 1) return arr[0] === val;
+  // if the end pointer is less than the start pointer then
+  // we have exhausted our search
+  if (end < start) return -1;
 
-  const index = parseInt(arr.length / 2, 10);
-  if (arr[index] > val) return searchRecursive(arr.slice(0, index), val);
-  else return searchRecursive(arr.slice(index, arr.length), val);
+  const index = parseInt(start + (end-start) / 2, 10);
+  // we found it!
+  if (arr[index] === val) return index;
+  // everything on the right side is going to be greater than `val`
+  // ignore the right side of the list
+  else if (arr[index] > val) return searchRecursive(arr, start, index-1, val);
+  // everything on the left side is going to be smaller than val
+  // ignore the left side of the list
+  else return searchRecursive(arr, index+1, end, val);
 }
 
 function search(arr, val) {
@@ -21,15 +28,16 @@ function search(arr, val) {
   let start = 0;
   let end = arr.length - 1;
   while(end >= start) {
-    const index = parseInt(start + (end - start)/2);
+    const index = parseInt(start + (end - start) / 2, 10);
 
-    // move the index down
-    // if (arr[end] < val) return false;
-    if (arr[index] === val) return true;
+    // we found it!
+    if (arr[index] === val) return index;
+    // everything on the right side is larger - ignore it
     else if (arr[index] > val) end = index - 1;
+    // everything on the left side if larger - ignore it
     else start = index + 1;
   }
-  return false;
+  return -1;
 }
 
 module.exports = {
