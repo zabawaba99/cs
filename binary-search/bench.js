dd582e1ae41ff2a150f8c450e92b8e767ee6c888
Steven Berlanga <zabawaba99@gmail.com>
2017-10-28 10:23:18 -0700

    adding in js benchmarking

---
+++ binary-search/bench.js
@@ -0,0 +1,26 @@
+const Benchmark = require('benchmark');
+
+const binarySearch = require('./index');
+const tests = require('./bench.json');
+
+tests.forEach((test) => {
+  const suite = new Benchmark.Suite;
+  // add tests
+  suite.add(`binarySearch#${test.name}#iterative`, function() {
+    const index = binarySearch.search(test.array, test.value);
+    // if (index !== test.index) throw new Error('incorrect value found');
+  })
+  .add(`binarySearch#${test.name}#recursive`, function() {
+    const index = binarySearch.searchRecursive(test.array, test.value);
+    // if (index !== test.index) throw new Error('incorrect value found');
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run();
+})
